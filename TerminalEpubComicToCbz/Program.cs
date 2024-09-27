21bb909b4178cd0d96c4cc587c0034c0bbe5bb95
Chris82111 <75613723+Chris82111@users.noreply.github.com>
2024-09-27 22:53:30 +0200

    Terminal program added

--- TerminalEpubComicToCbz/Program.cs
+++ TerminalEpubComicToCbz/Program.cs
@@ -1,14 +1,61 @@
 ﻿using Chris82111.Domain.Helper;
+using Chris82111.EpubConvertion;
+using SkiaSharp;
 using System.Globalization;
 
 namespace Chris82111.TerminalEpubComicToCbz
 {
     internal class Program
     {
+        private static string red = Console.IsOutputRedirected ? "" : "\x1b[31m";
+        private static string yellow = Console.IsOutputRedirected ? "" : "\x1b[0;33m";
+        private static string normal = Console.IsOutputRedirected ? "" : "\x1b[0m"; 
+
+        static void Help(string[] args)
+        {
+            Console.WriteLine("usage: ./TerminalEpubComicToCbz [-h help] [-i input]");
+            Console.WriteLine("");
+            foreach (var arg in args)
+            {
+                Console.WriteLine($"[{yellow}info{normal}] {arg}");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("No front end is available.");
+            var input = "";
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch(args[i])
+                {
+                    case "-i":
+                    case "--input":
+                        if(++i < args.Length)
+                        {
+                            input = args[i];
+                        }
+                        break;
+                    case "-h":
+                    case "--help":
+                        Help(args);
+                        return;
+                    default:
+                        break;
+                }
+            }
+
+            try
+            {
+                var epub = new FileInfo(input);
+                using (var epubToCbz = new EpubComicToCbz(epub))
+                {
+                    epubToCbz.Convert();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"[{red}fail{normal}] {ex}");
+            }
         }
     }
 }
