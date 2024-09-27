015c6fcf0b7d1f735cfc57443ce71a2fd7d41c31
Chris82111 <75613723+Chris82111@users.noreply.github.com>
2024-09-27 21:44:26 +0200

    Added Epub project

---
+++ Epub/Dto/MetaDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Chris82111.Epub.Dto
+{
+    public class MetaDto
+    {
+        public double Version { get; set; } = -1;
+        public string? Title { get; set; } = null;
+        public string? Creator { get; set; } = null;
+        public string? Publisher { get; set; } = null;
+        public DateTime? Date { get; set; } = null;
+        public string? Language { get; set; } = null;
+        public string? Identifier { get; set; } = null;
+    }
+}
