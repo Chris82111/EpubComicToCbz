2014d13bfc0c500bd5a4cbf2a23b4046cd02a791
Chris82111 <75613723+Chris82111@users.noreply.github.com>
2024-09-29 14:30:51 +0200

    Parameter input revised 2nd

--- TerminalEpubComicToCbz/Program.cs
+++ TerminalEpubComicToCbz/Program.cs
@@ -16,12 +16,12 @@ namespace Chris82111.TerminalEpubComicToCbz
             var nameOfExecutable = System.Diagnostics.Process.GetCurrentProcess().MainModule?.FileName ?? System.AppDomain.CurrentDomain.FriendlyName;
             nameOfExecutable = Path.GetFileName(nameOfExecutable);
 
+            // https://stackoverflow.com/questions/9725675/is-there-a-standard-format-for-command-line-shell-help-text
             Console.WriteLine(
-                $"usage: ./{nameOfExecutable} [-h -? help] [-i input] [--]" + newLine +
+                $"usage: ./{nameOfExecutable} [--help] [<--input inputFile>] <inputFile>" + newLine +
                 "" + newLine +
-                "  -h, -? --help : Shows this help" + newLine +
-                "  -i, --input   : The input file or directory" + newLine +
-                "  --            : Ends the input of commands" + newLine
+                "  --help, -h, -? : Shows this help" + newLine +
+                "  --input, -i,   : The input file or directory follows" + newLine
             );
 
             for (int i = 0; i < args.Length; i++)
@@ -30,6 +30,17 @@ namespace Chris82111.TerminalEpubComicToCbz
             }
         }
 
+        /// <summary>
+        ///         Extracts a value from the specified parameter that begins
+        /// <br/>   with the search string (flag/switch). An equals sign between
+        /// <br/>   the search string and the value is permitted.
+        /// </summary>
+        /// <param name="parameter">Contains the element and value to be searched for</param>
+        /// <param name="search">The name of the flag/switch</param>
+        /// <param name="handler">
+        ///         A handler that is called when a value is found, the found 
+        /// <br/>   value is the input parameter</param>
+        /// <returns>null if the search string is not found</returns>
         static string? ReadValue(string parameter, string search, Action<string>? handler = null)
         {
             string? value = null;
@@ -60,12 +71,6 @@ namespace Chris82111.TerminalEpubComicToCbz
 
         static void Main(string[] args)
         {
-            if (0 == args.Length)
-            {
-                Help(args);
-                return;
-            }
-
             var inputs = new List<string>();
             for (int i = 0; i < args.Length; i++)
             {                
@@ -83,14 +88,20 @@ namespace Chris82111.TerminalEpubComicToCbz
                     case "--help":
                         Help(args);
                         return;
-                    case "--":
-                        i = args.Length;
-                        break;
                     default:
-                        ReadValue(args[i], "-i",      (value) => inputs.Add(value));
-                        ReadValue(args[i], "--input", (value) => inputs.Add(value));
+                        if(null != ReadValue(args[i], "-i",      (value) => inputs.Add(value))) { break; }
+                        if(null != ReadValue(args[i], "--input", (value) => inputs.Add(value))) { break; }
+                        inputs.Add(args[i]);
                         break;
                 }
+            }
+            
+            if (0 == inputs.Count)
+            {
+                Help(args);
+                Console.WriteLine("No parameter found, press key to continue.");
+                Console.ReadKey();
+                return;
             }
 
             try
